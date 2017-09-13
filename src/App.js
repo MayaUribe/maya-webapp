fb0b3c768e1beab2b8134ef96c0d55f61d436a2a
Maria A Uribe <mariale.uribe@gmail.com>
2017-09-13 14:19:39 -0400

    Changing texts for index page.

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Maya Uribe</h2>
         </div>
         <p className="App-intro">
-          Hello World!
+          This site is under construction :)
         </p>
       </div>
     );
