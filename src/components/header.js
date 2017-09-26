b73c0a3ced928b1b2adc48ca3d8df95f30153c6e
Maria A Uribe <mariale.uribe@gmail.com>
2017-09-26 16:33:22 -0400

    Adjusting styles in general.

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import HeaderMenu from "./headerMenu";
+import $ from 'jquery';
 
 class Header extends Component {
+  _handleGoToBottom() {
+    $("html, body").animate({
+      scrollTop: $(document).height()
+    }, 1000);
+  }
+
   _renderHeaderIntro() {
     return (
       <header className="masthead">
@@ -9,7 +16,7 @@ class Header extends Component {
           <div className="intro-text">
             <div className="intro-heading">I'm a Software Developer</div>
             <div className="intro-lead-in">Hi there! My name is Maria Alejandra and I'm a Software Developer, I’m passionate about what I do, I’m a self taught person, and I’m always willing to learn new things.</div>
-            <a className="btn btn-xl btn-md btn-xs js-scroll-trigger" href="#footer">Contact Me</a>
+            <a className="btn btn-xl btn-md btn-xs js-scroll-trigger contact-me-btn" onClick={this._handleGoToBottom.bind(this)}>Contact Me</a>
           </div>
         </div>
       </header>
